1ee3864bb4bc4c5301e1d8a5d3a1026d50bd37ee
jassik23 <jassik2002@mail.ru>
2023-11-22 02:55:30 +1000

    Create User

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class UserQuerySet(models.QuerySet):
+    def for_viewset(self) -> 'UserQuerySet':
+        return self.prefetch_related('todos')
+
+
+class User(models.Model):
+
+    objects = models.Manager.from_queryset(UserQuerySet)()
+
+    username = models.CharField(max_length=255)
